13a734278e788ecb56312be2856cd1852f636a75
JonathanLaaksonen <jonathan.laaksonen@gmail.com>
2024-03-07 15:08:58 +0200

    eka commit

---
+++ src/main/java/bookstore/bookstore/BookRepository.java
@@ -0,0 +1,7 @@
+package bookstore.bookstore;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    
+}
--- src/main/java/bookstore/bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/bookstore/BookstoreApplication.java
@@ -1,7 +1,9 @@
 package bookstore.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,5 +11,14 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner initDatabase(BookRepository repository) {
+        return args -> {
+            repository.save(new Book("Title1", "Author1", 2000, "ISBN1", 19.99));
+            repository.save(new Book("Title2", "Author2", 2001, "ISBN2", 29.99));
+            // Add as many books as you want here
+        };
+    }
 
 }
