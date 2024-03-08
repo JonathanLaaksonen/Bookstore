741d3d5d4fc797732b5e1e620daa0f61265f25f1
JonathanLaaksonen <jonathan.laaksonen@gmail.com>
2024-03-08 15:49:24 +0200

    kolmas commit

--- src/main/java/bookstore/bookstore/BookController.java
+++ src/main/java/bookstore/bookstore/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -21,4 +24,28 @@ public class BookController {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist"; 
     }
+
+    @GetMapping("/addbook")
+    public String showAddBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+}
+@PostMapping("/addbook")
+    public String addBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+}
+@PostMapping("/booklist")
+    public String saveBook(@ModelAttribute("book") Book book) {
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+@GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long bookId) {
+        if (bookId != null) {
+            bookRepository.deleteById(bookId);
+}
+return "redirect:/booklist";
+}
+
 }
--- src/main/java/bookstore/bookstore/BookstoreApplication.java
+++ src/main/java/bookstore/bookstore/BookstoreApplication.java
@@ -17,6 +17,7 @@ public class BookstoreApplication {
         return args -> {
             repository.save(new Book("How to be boss", "Bossman", 2023, "ISBN1", 19.99));
             repository.save(new Book("How to be hero", "Sankari", 2024, "ISBN2", 29.99));
+            repository.save(new Book("Taistelijat", "Superman", 2024, "ISBN3", 26.99));
             // Add as many books as you want here
         };
     }
